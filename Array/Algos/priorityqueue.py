4ca96a6fae8e2ef3083b56d860c924a0dfcc4136
Nikhil Yadav <ny619619@gmail.com>
2024-11-22 12:29:29 +0530

    name changed

--- Array/Algos/priorityqueue.py
+++
@@ -1,9 +0,0 @@
-class Priority:
-    def __init__(self):
-        self.items = []
-        self.item_count = 0
-    def is_empty(self):   
-        return self.item_count == 0
-    def push(self,data,priority):
-        index = 0
-        while index < len(self.items) and             
