2a3587f78d090f9422830b8d01fdbcc579e81aa8
Nikhil Yadav <ny619619@gmail.com>
2024-12-03 19:20:40 +0530

    classes and object and self keyword

---
+++ basics/classesandobject.py
@@ -0,0 +1,11 @@
+class Employee:
+    def info(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+e1 = Employee()
+
+e1.name = "Nikhil Yadav"
+e1.salary = 90000000000000000
+
+print(e1.name,e1.salary)
