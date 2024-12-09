90d6d81ca2ceecf54be449b3f8e1e1cdbb2929a7
Nikhil Yadav <ny619619@gmail.com>
2024-12-09 12:35:24 +0530

    added one more way to do code

--- Array/66_plusone.py
+++ Array/66_plusone.py
@@ -1,3 +1,24 @@
+
+#Easy way to handle this 
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits)-1
+        while i >= 0 :
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+            i -=1
+        return [1]+digits
+
+
+
+
+
+
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
