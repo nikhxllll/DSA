66459d9e569f8d4c9179220b3d10fe1bec67f109
Nikhil Yadav <ny619619@gmail.com>
2024-12-10 13:38:38 +0530

    sum method

--- Array/268_missingnumber.py
+++ Array/268_missingnumber.py
@@ -3,3 +3,7 @@ class Solution:
         n = len(nums)
         expectedSum = n*(n+1) // 2
         return expectedSum - sum(nums) 
+    
+
+    # you can used XOR also
+    # but this is the simplest soln to this problem
