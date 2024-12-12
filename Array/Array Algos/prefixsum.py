26c9b562656a172822f3252a245958c9246561ba
Nikhil Yadav <ny619619@gmail.com>
2024-12-12 13:07:02 +0530

    optimized

--- Array/Array Algos/prefixsum.py
+++ Array/Array Algos/prefixsum.py
@@ -1,7 +1,7 @@
 def prefix(nums):
     p = []
 
-    for i in range(len(nums)):
+    for i in range(len(nums)):  # we can avoid if and else condition by checking for loop with 1 ,len(nums)
         if i > 0:
             nums[i] += nums[i-1]
             p.append(nums[i])
