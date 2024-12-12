fc793123a74940b0db1ddd298bf8c0003f97b650
Nikhil Yadav <ny619619@gmail.com>
2024-12-12 13:13:06 +0530

    done

---
+++ Array/303_rangesumquery.py
@@ -0,0 +1,24 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        total = 0
+        for i in nums:
+            total += i
+            self.prefix.append(total)
+
+
+    def sumRange(self, left: int, right: int) -> int:
+        rightSum = self.prefix[right]
+        
+        if left > 0:
+            leftSum  = self.prefix[left-1] 
+        else:
+            leftSum = 0
+        
+        return rightSum - leftSum
+
+# Prefix sum is used
+
+
+
