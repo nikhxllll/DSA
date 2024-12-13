49c836dc6592b44257052dcab6a775b620d796d2
Nikhil Yadav <ny619619@gmail.com>
2024-12-13 12:39:39 +0530

    done

---
+++ Array/1991_findmiddleindex.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        leftsum = 0
+        total = sum(nums)
+        for i in range(len(nums)):
+            rightsum = total - nums[i] - leftsum
+            if leftsum == rightsum:
+                return i
+            leftsum += nums[i]
+
+        return -1
