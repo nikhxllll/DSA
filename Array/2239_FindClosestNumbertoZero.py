dd428840d724caa2c2ce4ab47641c7ab4519451d
Nikhil Yadav <ny619619@gmail.com>
2024-12-16 14:19:23 +0530

    done

---
+++ Array/2239_FindClosestNumbertoZero.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        min = nums[0]
+
+        for n in nums:
+            if abs(n) < abs(min):
+                min = n
+
+        if min < 0 and abs(min) in nums:
+            return abs(min)
+        else:
+            return min
+        
+        # return abs(min)
+
+        
