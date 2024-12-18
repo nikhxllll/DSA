6350048fa3a3e6321d599820007c9a7e18e7a670
Nikhil Yadav <ny619619@gmail.com>
2024-12-18 18:01:52 +0530

    is empty done

--- Array/Algos/doublylinkedlist.py
+++ Array/Algos/doublylinkedlist.py
@@ -1,8 +1,15 @@
 class Node:
-    def __init__(self,prev,item,next):
+    def __init__(self,prev=None,item= None,next= None):
         self.prev = prev
         self.item = item
         self.next = next
+class Dll:
+    def __init__(self,start=None):
+        self.start = start
+    def is_empty(self):
+        return self.start == None
+    
 
         
+
     
