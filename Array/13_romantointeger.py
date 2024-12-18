da6a35a4af91e515b4e5a95bd5c9fce5ff792d74
Nikhil Yadav <ny619619@gmail.com>
2024-12-18 13:48:11 +0530

    done

---
+++ Array/13_romantointeger.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+        'I': 1,    # 1
+        'V': 5,    # 5
+        'X': 10,   # 10
+        'L': 50,   # 50
+        'C': 100,  # 100
+        'D': 500,  # 500
+        'M': 1000  # 1000
+        }
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
+    
+    # hashset is used
