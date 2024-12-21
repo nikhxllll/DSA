955c7558c54976a603c3f1e25b3578a917ab7bbd
Nikhil Yadav <ny619619@gmail.com>
2024-12-21 11:40:46 +0530

    class defined

---
+++ Array/Algos/circulardll.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,prev = None,item= None,next = None):
+        self.prev = prev
+        self.item = item 
+        self.next = next
+class Cdll:
+    def __init__(self,start = None):
+        self.start = start
+
+    
+    def insert_at_start(self,data):
