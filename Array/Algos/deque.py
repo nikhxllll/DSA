06b6a9e2ff19d1e2d3244f59d4d7f5218034d59a
Nikhil Yadav <ny619619@gmail.com>
2024-12-25 11:18:08 +0530

    deque done

--- Array/Algos/deque.py
+++ Array/Algos/deque.py
@@ -1,13 +1,32 @@
 class Deque:
     def __init__(self):
         self.items = []
-        self.front = 0
-        self.rear = 0
-        self.item_count = 0
-
     def is_empty(self):
-        return self.item_count == 0
+        return len(self.items) == 0
     def insert_front(self,data):
-        self.items.append(data)
+        self.items.insert(0,data)
     def insert_rear(self,data):
-        
+        self.items.append(data)
+    def delete_front(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        else:
+            self.items.pop(0)
+    def delete_rear(self):
+        if self.is_empty():
+            raise IndexError("Queue is emptyyy")
+        else:
+            self.items.pop(-1)
+    def get_front(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        else:
+            return self.items[0]
+    def get_rear(self):
+        if self.is_empty():
+            raise IndexError("Queue is emptyyy")
+        else:
+            return self.items[-1]
+    def size(self):
+        return len(self.items)
+    
