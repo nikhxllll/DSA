a977aa61a7f3e658347625c884e31d1d19f22989
Nikhil Yadav <ny619619@gmail.com>
2024-12-26 17:47:41 +0530

    push done

--- Array/Algorithms/priorityusingLL.py
+++ Array/Algorithms/priorityusingLL.py
@@ -9,6 +9,16 @@ class PriorityQueue:
         self.item_count = 0
     def push(self,data,priority):
         n = Node(data,priority)
-        temp = self.start
-        if not self.start and self.start.priority > priority:
+        if not self.start or self.start.priority < priority:
+            n.next = self.start
+            self.start = n
+        else:
+            temp = self.start
+            while temp.next != None and temp.next.priority <= priority:
+                temp=temp.next
+            n.next = temp.next
+            temp.next = n
+    
+
+        
 
