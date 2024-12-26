7c6bb8352cac6685c45be20d2d8dc15ea9e594f4
Nikhil Yadav <ny619619@gmail.com>
2024-12-26 17:59:14 +0530

    priority queue using linked list done

--- Array/Algorithms/priorityusingLL.py
+++ Array/Algorithms/priorityusingLL.py
@@ -37,9 +37,12 @@ q.push("Ny",10)
 q.push("Jy",20)
 q.push("Sy",30)
 q.push("Ky",40)
-
+print(q.size())
 while not q.is_empty():
+    
     print(q.pop())
+    print("Deleted:",q.size())
+print(q.size())
     
         
             
