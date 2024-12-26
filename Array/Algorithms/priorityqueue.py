dc3e7a972b5cf20c09dc4813fd1d26d595a892d5
Nikhil Yadav <ny619619@gmail.com>
2024-12-26 13:01:54 +0530

    priority queue done

--- Array/Algorithms/priorityqueue.py
+++ Array/Algorithms/priorityqueue.py
@@ -6,4 +6,13 @@ class Priority:
         return self.item_count == 0
     def push(self,data,priority):
         index = 0
-        while index < len(self.items)            
+        while index < len(self.items) and self.items[index][1]<= priority:
+            index +=1
+        self.items.insert(index,(data,priority))
+        self.item_count +=1
+    def pop(self):
+        if not self.is_empty():
+            self.item_count -=1
+            return self.items.pop(0)[0]   
+    def size(self):
+        return self.item_count()
