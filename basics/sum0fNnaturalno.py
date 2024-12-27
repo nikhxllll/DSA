af176f738a5a75db6b75f4be61b5bb60963c0a0b
Nikhil Yadav <ny619619@gmail.com>
2024-12-27 12:58:06 +0530

    Done using recursion

--- basics/sum0fNnaturalno.py
+++ basics/sum0fNnaturalno.py
@@ -1,8 +1,18 @@
+# NORMAL WAY
+# def sum(n):
+    # summ =0
+#     while n > 0:
+#         summ += n
+#         n -=1
+#     return summ
+# s = sum(-6)
+# print(s)
+# Recursion WAY
 def sum(n):
-    summ =0
-    while n > 0:
-        summ += n
-        n -=1
-    return summ
-s = sum(3)
-print(s)
+    if n == 1:
+        return 1
+    else:
+        s =n+ sum(n-1)
+        return s
+print(sum(3))
+
