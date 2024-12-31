d748af4f28966c78f0f6ae493672476a5a27bf27
Nikhil Yadav <ny619619@gmail.com>
2024-12-31 14:06:10 +0530

    insert done

--- Array/Algorithms/binarySearchTree.py
+++ Array/Algorithms/binarySearchTree.py
@@ -7,5 +7,12 @@ class Bst:
     def __init__(self):
         self.root = None
     def insert(self,data):
-        n = Node(data)
-        
+        self.root = self.rinsert(self.root,data)
+    def rinsert(self,root,data):
+        if root == None:
+            return Node(data)
+        if data < root.item:
+            root.left = self.rinsert(root.left,data)
+        elif data > root.item:
+            root.right = self.rinsert(root.right,data)
+        return root
