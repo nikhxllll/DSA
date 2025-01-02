2c74a644775a1c6638389445bcc78a2457077d5d
Nikhil Yadav <ny619619@gmail.com>
2025-01-02 18:20:40 +0530

    ADD EDGE

--- Array/Algorithms/adjacency_matrix.py
+++ Array/Algorithms/adjacency_matrix.py
@@ -2,4 +2,9 @@ class Graph:
     def __init__(self,vno):
         self.vertex_count = vno
         self.adj_matrix = [[0]*vno for i in range(vno)]
-    
+    def add_edge(self,u,v,weight = 1):
+        if 0<=u< self.vertex_count and 0<=v < self.vertex_count:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+        else:
+            print("Invalid ")
