0027532c293fc9a629af072e00ba200f4baaf364
Nikhil Yadav <ny619619@gmail.com>
2025-01-03 14:08:25 +0530

    adjacency list done

--- Array/Algorithms/adj_list.py
+++ Array/Algorithms/adj_list.py
@@ -10,8 +10,8 @@ class Graph:
             print("Invalid Vertex")
     def remove_edge(self,u,v):
         if 0 <= u < self.vertex_count and 0 <= v < self.vertex_count:
-            self.vertex_count[u] = [(vertex,weight) for vertex,weight in self.adj_list[u] if vertex != v]
-            self.vertex_count[v] = [(vertex,weight) for vertex,weight in self.adj_list[v] if vertex != u]
+            self.adj_list[u] = [(vertex,weight) for vertex,weight in self.adj_list[u] if vertex != v]
+            self.adj_list[v] = [(vertex,weight) for vertex,weight in self.adj_list[v] if vertex != u]
         else:
             print("Invaaliv vertex")
     def has_edge(self,u,v):
@@ -24,3 +24,13 @@ class Graph:
         for vertex,n in self.adj_list.items():
             print("V",vertex,":",n)
 # Driver code
+g = Graph(5)
+g.add_edge(0,1)
+g.add_edge(1,2)
+g.add_edge(1,3)
+g.add_edge(2,4)
+g.add_edge(3,4)
+# g.print_list()
+# g.remove_edge(0,1)
+g.print_list()
+print(g.has_edge(0,1))
