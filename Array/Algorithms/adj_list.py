446491ce49f49bc871f945e791742581f2b94aff
Nikhil Yadav <ny619619@gmail.com>
2025-01-03 13:05:03 +0530

    adding of edges done

--- Array/Algorithms/adj_list.py
+++ Array/Algorithms/adj_list.py
@@ -1,2 +1,11 @@
 class Graph:
-    def __init__(self,):
+    def __init__(self,vno):
+        self.vertex_count = vno
+        self.adj_list = {v:[] for v in range(vno)}
+    def add_edge(self,u,v,weight = 1):
+        if 0<=u<self.vertex_count and 0<= v < self.vertex_count:
+            self.adj_list[u].append((v,weight))
+            self.adj_list[v].append((u,weight))
+        else:
+            print("Invalid Vertex")
+    
