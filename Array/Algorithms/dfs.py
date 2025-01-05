54794b7068ffafe226e52e63719d3f6f1db5ad5d
Nikhil Yadav <ny619619@gmail.com>
2025-01-05 10:50:17 +0530

    dfs fun done

--- Array/Algorithms/dfs.py
+++ Array/Algorithms/dfs.py
@@ -8,3 +8,15 @@ class Dfs:
             self.items[v] = []
         self.items[u].append[v]
         self.items[v].append[u]
+    def dfs(self,start):
+        visited = set()
+        stack = [start]
+        while stack:
+            node = stack.pop()
+            print(node,end=" ")
+            visited.add(node)
+            for nei in self.items[node]:
+                visited.add(nei)
+                stack.append(nei)
+d = Dfs()
+
