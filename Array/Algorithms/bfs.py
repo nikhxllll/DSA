af51d3bae94b5522b3b3838cb3d820d8540edaec
Nikhil Yadav <ny619619@gmail.com>
2025-01-06 15:23:59 +0530

    add egde done

--- Array/Algorithms/bfs.py
+++ Array/Algorithms/bfs.py
@@ -1,5 +1,13 @@
 class BFS:
     def __init__(self):
         self.graph = {}
+    def add_edge(self,u,v):
+        if u not in self.graph:
+            self.graph[u] = []
+            self.graph[u].append(v)
+        if v not in self.graph:
+            self.graph[v] = []
+            self.graph[v].append(u)
     
+
             
