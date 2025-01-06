8fbdf7a56b55055a3b0ab9d2b2e2ab344a4f74e1
Nikhil Yadav <ny619619@gmail.com>
2025-01-06 15:23:41 +0530

    bfs init

--- Array/Algorithms/bfs.py
+++ Array/Algorithms/bfs.py
@@ -1,22 +1,5 @@
 class BFS:
     def __init__(self):
         self.graph = {}
-    def add_edge(self,u,v):
-        if u not in self.graph:
-            self.graph[u] = []
-            self.graph[u].append(v)
-        if v not in self.graph:
-            self.graph[v] = []
-            self.graph[v].append(u)
-    def bfs(self,strt):
-        visited = set()
-        queue =[strt]
-        while queue:
-            node = queue.pop(0)
-            print(node,end = " ")
-            visited.add(node)
-            for nei in self.graph[node]:
-                if nei not in visited:
-                        queue.append(nei)
-
+    
             
