6d9e6af3c94c7ed8674d3b11c2dbdebf5ebf653c
Nikhil Yadav <ny619619@gmail.com>
2025-01-06 15:30:38 +0530

    Breadth first search done

--- Array/Algorithms/bfs.py
+++ Array/Algorithms/bfs.py
@@ -4,19 +4,25 @@ class BFS:
     def add_edge(self,u,v):
         if u not in self.graph:
             self.graph[u] = []
-            self.graph[u].append(v)
         if v not in self.graph:
             self.graph[v] = []
-            self.graph[v].append(u)
+        self.graph[u].append(v)
+        self.graph[v].append(u)
     def bfs(self,strt):
         visited = set()
         queue =[strt]
         while queue:
             node = queue.pop(0)
-            print(node,end = " ")
-            visited.add(node)
-            for nei in self.graph[node]:
-                if nei not in visited:
-                        queue.append(nei)
-
-            
+            if node not in visited:
+                print(node,end = " ")
+                visited.add(node)
+                for nei in self.graph[node]:
+                    if nei not in visited:
+                            queue.append(nei)
+# Driver Code
+d = BFS()
+d.add_edge(1,2)
+d.add_edge(1,3)
+d.add_edge(2,4)
+d.add_edge(2,5)
+print(d.bfs(1))
